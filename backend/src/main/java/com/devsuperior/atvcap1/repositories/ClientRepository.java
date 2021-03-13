1617b6a5e42097b86f59927a665d6401585b13b1
Pedro Ferraresi <ferraresi.pedro@gmail.com>
2021-03-13 16:15:19 -0300

    Implemented ClientRepository class

--- backend/src/main/java/com/devsuperior/atvcap1/repositories/ClientRepository.java
+++ backend/src/main/java/com/devsuperior/atvcap1/repositories/ClientRepository.java
@@ -1,5 +1,9 @@
 package com.devsuperior.atvcap1.repositories;
 
-public class ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.atvcap1.entities.Client;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
