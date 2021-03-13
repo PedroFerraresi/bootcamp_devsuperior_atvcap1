90e0b241a79de59e62402d243f11c6b5d3c49ea1
Pedro Ferraresi <ferraresi.pedro@gmail.com>
2021-03-13 16:16:49 -0300

    Adjusted annotations on Client Entity and Repository classes

--- backend/src/main/java/com/devsuperior/atvcap1/repositories/ClientRepository.java
+++ backend/src/main/java/com/devsuperior/atvcap1/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.atvcap1.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.atvcap1.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
